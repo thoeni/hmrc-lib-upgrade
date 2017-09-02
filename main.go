e0ce466136ae1a64d3695cca5df77cb9e8637944
thoeni <thoeni@gmail.com>
2017-09-02 09:42:42 +0100

    Added support to Nexus

--- main.go
+++ main.go
@@ -10,9 +10,11 @@ import (
 	"sync"
 	"time"
 	"flag"
+	"sort"
 )
 
-type bintrayResp struct {
+type searchResp struct {
+	Source string
 	Name string
 	Updated string
 }
@@ -75,22 +77,19 @@ func getLatestVersion(lib []string, errors *[]string, wg *sync.WaitGroup) {
 	libName := lib[1]
 	libCurVersion := lib[2]
 
-	url := fmt.Sprintf("https://api.bintray.com/packages/hmrc/releases/%s/versions/_latest", libName)
-	r, err := http.Get(url)
-	if err != nil {
-		*errors = append(*errors, fmt.Sprintf("%s [%s] - Couldn't get url: %s because of error [%v]", libName, libCurVersion, url, err))
-	}
-	if r.StatusCode != http.StatusOK {
-		*errors = append(*errors, fmt.Sprintf("%s [%s] - Couldn't get url: %s because of error [%s]", libName, libCurVersion, url, r.Status))
-	}
+	var resp = searchResp{}
+	var err error
 
-	b, err := ioutil.ReadAll(r.Body)
+	resp, err = getFromBintray(libName)
 	if err != nil {
-		*errors = append(*errors, fmt.Sprintf("%s [%s] - Couldn't read body of response from: %s because of error [%v]", libName, libCurVersion, url, err))
-	}
-	var resp bintrayResp
-	if err := json.Unmarshal(b, &resp); err != nil {
-		*errors = append(*errors, fmt.Sprintln("Error while unmarshalling", string(b), err))
+		if err.Error() != "[404]" {
+			*errors = append(*errors, fmt.Sprintf("%s [%s] - Couldn't get version because of error %v", libName, libCurVersion, err))
+		} else {
+			resp, err = getFromNexus(libName)
+			if err != nil {
+				*errors = append(*errors, fmt.Sprintf("%s [%s] - Couldn't get version because of error %v", libName, libCurVersion, err))
+			}
+		}
 	}
 
 	printLine(libName, libCurVersion, resp, len(*errors))
@@ -98,14 +97,79 @@ func getLatestVersion(lib []string, errors *[]string, wg *sync.WaitGroup) {
 	wg.Done()
 }
 
-func printLine(libName string, libCurVersion string, br bintrayResp, errId int) {
+func getFromBintray(libName string) (searchResp, error) {
+	url := fmt.Sprintf("https://api.bintray.com/packages/hmrc/releases/%s/versions/_latest", libName)
+	r, err := http.Get(url)
+	if err != nil {
+		return searchResp{}, err
+	}
+
+	if r.StatusCode != http.StatusOK {
+		return searchResp{}, fmt.Errorf("[%d]", r.StatusCode)
+	}
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return searchResp{}, err
+	}
+
+	var resp searchResp
+	if err := json.Unmarshal(b, &resp); err != nil {
+		return searchResp{}, err
+	}
+
+	resp.Source = "Bintray"
+	return resp, nil
+}
+
+func getFromNexus(libName string) (searchResp, error) {
+	url := fmt.Sprintf("https://nexus-dev.tax.service.gov.uk/content/repositories/hmrc-releases/uk/gov/hmrc/%s_2.11/", libName)
+	r, err := http.Get(url)
+	if err != nil {
+		return searchResp{}, err
+	}
+
+	if r.StatusCode != http.StatusOK {
+		return searchResp{}, fmt.Errorf("[%d]", r.StatusCode)
+	}
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return searchResp{}, err
+	}
+
+	return parseLatestNexus(b)
+}
+
+func parseLatestNexus(body []byte) (searchResp, error) {
+	r, _ := regexp.Compile("https:\\/\\/nexus-dev.tax.service.gov.uk\\/content\\/repositories\\/hmrc-releases\\/uk\\/gov\\/hmrc\\/.*?>(.*?)\\/")
+	c := string(body)
+	m := r.FindAllStringSubmatch(c, -1)
+
+	if len(m) == 0 {
+		return searchResp{}, fmt.Errorf("No matches.")
+	}
+
+	var versions []string = make([]string, len(m))
+	for i, v := range m {
+		versions[i] = v[1]
+	}
+	sort.Strings(versions)
+	return searchResp{
+		Source: "Nexus",
+		Name: versions[len(versions)-1],
+	}, nil
+}
+
+func printLine(libName string, libCurVersion string, br searchResp, errId int) {
 
 	libLatestVersion := br.Name
-	libLatestUpdate, _ := time.Parse(dateLayout, br.Updated)
+	//libLatestUpdate, _ := time.Parse(dateLayout, br.Updated)
+	//updFmt := libLatestUpdate.Format("2006-01-02")
 
 	switch {
 	case libLatestVersion == "": color.Yellow("|%30s|%10s|%10s|%12s|%8s|\n", libName, libCurVersion, fmt.Sprintf("err[%d]", errId), "", "")
-	case libLatestVersion > libCurVersion: color.Red("|%30s|%10s|%10s|%12s|%8s|\n", libName, libCurVersion, libLatestVersion, libLatestUpdate.Format("2006-01-02"), "[*]")
-	default: color.Green("|%30s|%10s|%10s|%12s|%8s|\n", libName, libCurVersion, libLatestVersion, libLatestUpdate.Format("2006-01-02"), "")
+	case libLatestVersion > libCurVersion: color.Red("|%30s|%10s|%10s|%12s|%8s|\n", libName, libCurVersion, libLatestVersion, br.Source, "[*]")
+	default: color.Green("|%30s|%10s|%10s|%12s|%8s|\n", libName, libCurVersion, libLatestVersion, br.Source, "")
 	}
 }
